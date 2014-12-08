47d8070313a28bac205a82013f7b68f8739a02b4
nat <nat.lownes@gmail.com>
2014-12-08 16:34:17 -0500

    refresh on click

--- py3status_modules/lastfm.py
+++ py3status_modules/lastfm.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from time import time
 
@@ -11,6 +12,18 @@ class Py3status(object):
   base_uri = 'http://ws.audioscrobbler.com/2.0/'
   api_key  = '6760190ec250ec3382636898fc4e53e0'
   user     = 'crabwhittaker'
+
+  def now_playing(self, json, i3status_config):
+    response = {
+      'cached_until':  self._cached_until(),
+      'full_text':     self._now_playing_formatted(),
+      'name':          'last.fm'
+    }
+
+    return (self.index, response)
+
+  def on_click(self, i3status_output_json, i3status_config, event):
+    os.system("killall -USR1 py3status")
 
   def _params(self):
     return {
@@ -40,11 +53,31 @@ class Py3status(object):
     now_playing = self._get_now_playing()
     return '%s - %s' % (now_playing['artist'], now_playing['track'],)
 
-  def now_playing(self, json, i3status_config):
-    response = {
-      'cached_until':  self._cached_until(),
-      'full_text':     self._now_playing_formatted(),
-      'name':          'last.fm'
+  def _params(self):
+    return {
+      'api_key':      self.api_key,
+      'format':       'json',
+      'limit':        2,
+      'method':       'user.getrecenttracks',
+      'now_playing':  True,
+      'user':         self.user,
     }
 
-    return (self.index, response)
+
+  def _cached_until(self):
+    return time() + 120
+
+  def _get_now_playing(self):
+    request = requests.get(self.base_uri, params=self._params())
+    request.json()
+
+    now_playing = request.json()['recenttracks']['track'][0]
+    return {
+      'artist': now_playing['artist']['#text'],
+      'track':  now_playing['name']
+    }
+
+  def _now_playing_formatted(self):
+    now_playing = self._get_now_playing()
+    return '%s - %s' % (now_playing['artist'], now_playing['track'],)
+
