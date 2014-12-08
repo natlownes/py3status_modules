b3b4d0503caa7c6dff22bb21632a5210ca72cccb
nat <nat.lownes@gmail.com>
2014-12-08 13:50:54 -0500

    lastfm notes

---
+++ py3status_modules/lastfm.py
@@ -0,0 +1,6 @@
+
+
+class Py3status(object):
+  '''
+  http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=crabwhittaker&api_key=6760190ec250ec3382636898fc4e53e0&format=json&now_playing=true&limit=2
+  '''
