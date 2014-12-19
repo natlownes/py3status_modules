0f8a738426953044eb932cc8ab2c568161f4d0e0
nat <nat.lownes@gmail.com>
2014-12-19 07:58:11 -0500

    add location file
    if ~/.location.json exists, use that instead of hitting ipinfo.json -
    useful for when ipinfo.json can't return a decent location

--- py3status_modules/location.py
+++ py3status_modules/location.py
@@ -1,10 +1,19 @@
 import requests
 
+from json import loads
+from os import path
+
 
 class Location(object):
-  url = 'http://ipinfo.io/json'
+  '''
+  use the ipinfo.io service to return a location object
+  if a $HOME/.location.json exists, just return that
+  '''
 
   def _get_location(self):
+    url                = 'http://ipinfo.io/json'
+    home_dir           = path.expanduser("~")
+    location_file_path = path.join(home_dir, ".location.json")
     '''
     return object:
       {
@@ -13,11 +22,25 @@ class Location(object):
         'zipcode':    19125,
       }
     '''
-    r = requests.get(self.url)
-    response = r.json()
-    latitude, longitude = response['loc'].split(',')
-    return {
-      'latitude':   latitude,
-      'longitude':  longitude,
-      'zipcode':    response.get('postal', '')
-    }
+
+    def has_location_file():
+      return path.isfile(location_file_path)
+
+    def request():
+      r = requests.get(url)
+      response = r.json()
+      latitude, longitude = response['loc'].split(',')
+      return {
+        'latitude':   latitude,
+        'longitude':  longitude,
+        'zipcode':    response.get('postal', '')
+      }
+
+    def read_local():
+      with open(location_file_path, 'r') as f:
+        return loads(f.read())
+
+    if has_location_file():
+      return read_local()
+    else:
+      return request()
